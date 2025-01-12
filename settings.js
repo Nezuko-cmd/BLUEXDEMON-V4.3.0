29328a6a80e5beb7efd79342693729d44f534d46
Nezuko-cmd <onlineservet6@gmail.com>
2025-01-12 20:03:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2348086636718"
 
 
 
 
 //DO NOT EDIT OR MODD
-global.botNumber = pairingNumber
+global.botNumber = 2348086636718
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
@@ -22,14 +22,14 @@ global.autoswview = false
 global.public = true
 global.autoreact = false
 global.antidelete = false
-global.autotyping = false
+global.autotyping = true
 global.autoBio = true
 global.antiSpam = true
 global.sign = "𓃵"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
-global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.bankname = "Alaga karamat Oreoluwa"
+global.accnumber = "7019683483"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
